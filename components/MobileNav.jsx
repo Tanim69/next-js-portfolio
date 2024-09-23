1c8ff6d20ff3d7c999e041d75f78b7a8365bfac0
tanim <tanimkhan117@gmail.com>
2024-09-23 23:24:14 +0600

    navbar

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -40,12 +40,34 @@ const MobileNav = () => {
         <>
             <Sheet>
                 <SheetTrigger className='flex justify-center items-center'>
-                    <CiMenuFries className='text-[32px] text-accent'/>
+                    <CiMenuFries className='text-[32px] text-accent' />
                 </SheetTrigger>
                 <SheetContent className='flex flex-col '>
-                    <div>
-                        Logo
+                    <div className='mt-28 mb-4 text-center text-2xl  '>
+
+                        <Link href="/">
+
+                            <h1 className='text-4xl font-semibold'>Tanim <span className='text-accent'>Khan</span></h1>
+
+                        </Link>
+
                     </div>
+                    <nav className='flex flex-col justify-center items-center gap-8'>
+                        {links.map((link,index) => {
+                            return (
+                                <Link
+                                 href={link.path}
+                                    key={index}
+                                    className={`${
+                                        link.path ===  pathname && 
+                                        "text-accent border-b-2 border-accent "
+                                        } text-xl capitalize hover:text-accent transition-all`}
+                                        >
+                                            {link.name}
+                                </Link>
+                            );
+                        })}
+                    </nav>
                 </SheetContent>
             </Sheet>
         </>
